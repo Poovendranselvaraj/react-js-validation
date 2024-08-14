749ebdd2da2b3e58520ae4fe65aebc90cb85252a
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-08-14 17:51:45 +0530

    bug fixed

--- app/src/Register.js
+++ app/src/Register.js
@@ -52,7 +52,7 @@ const Register = () => {
         const v1=USER_REGEX.test(user);
         const v2=PWD_REGEX.test(pwd);
         
-        if(!v1 || v2){
+        if(!v1 || !v2){
             setErrMsg("Invalid Entry");
             return;
         }
