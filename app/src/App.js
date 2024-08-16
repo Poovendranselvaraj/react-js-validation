ab74ab3c5428e81d6cc8083f0e86f0e6d5848556
poove <poovendranselavaraj333@gmail.com>
2024-08-16 18:08:10 +0530

    3video 17.35

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,37 @@
 import Register from './Register';
 import Login from './Login';
-
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Missing from './components/Missing';
+import Unauthorized from './components/Unauthorized';
+import LinkPage from './components/LinkPage';
+import Lounge from './components/Lounge';
+import Editor from './components/Editor';
+import Admin from './components/Admin';
+import RequireAuth from './components/RequireAuth';
+import { Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+         {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="linkpage" element={<LinkPage />} />
+
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+          <Route path="lounge" element={<Lounge />} />
+          <Route path="editor" element={<Editor />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
