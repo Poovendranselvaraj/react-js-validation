b3a755bb7d9b0254a0b5373e04f7f22ee53c55c4
poove <poovendranselavaraj333@gmail.com>
2024-08-16 20:10:27 +0530

    3video 28.22

--- app/src/components/RequireAuth.js
+++ app/src/components/RequireAuth.js
@@ -1,17 +1,24 @@
-import {useLocation, Navigate, Outlet} from 'react-router-dom';
+import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import { jwtDecode } from "jwt-decode";
 
 const RequireAuth = ({ allowedRoles }) => {
-    const {auth} = useAuth();
+    const { auth } = useAuth();
     const location = useLocation();
+    // const decoded=auth?.accessToken?jwtDecode(auth.accessToken):undefined
+    // console.log("inside require auth",decoded)
+    // const roles=decoded?.UserInfo.roles||[];
+    // console.log("inside require auth",roles)
+
+
 
     return (
-        roles?.find(role => allowedRoles?.includes(role))
-            ? <Outlet/>
+        auth?.roles?.find(role => allowedRoles?.includes(role))
+            ? <Outlet />
             : auth?.user
-               ? <Navigate to="/unauthorized" state={{from:location}} replace />
-               : <Navigate to="/login" state={{from:location}} replace />
+                ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+                : <Navigate to="/login" state={{ from: location }} replace />
     );
 }
 
-export default RequireAuth;    
+export default RequireAuth;
