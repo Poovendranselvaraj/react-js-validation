51c00015f280a0247f0b3df57cb7a4a36060741d
poove <poovendranselavaraj333@gmail.com>
2024-08-19 19:43:19 +0530

    4th video bug fixed

--- app/src/components/RequireAuth.js
+++ app/src/components/RequireAuth.js
@@ -1,14 +1,14 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-// import { jwtDecode } from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
-    // const decoded=auth?.accessToken?jwtDecode(auth.accessToken):undefined
-    // console.log("inside require auth",decoded)
-    // const roles=decoded?.UserInfo.roles||[];
-    // console.log("inside require auth",roles)
+    const decoded=auth?.accessToken?jwtDecode(auth.accessToken):undefined
+    console.log("inside require auth",decoded)
+    const roles=decoded?.UserInfo.roles||[];
+    console.log("inside require auth",roles)
 
 
 
--- app/src/components/Users.js
+++ app/src/components/Users.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-import useAxoisPrivate from "../hooks/useAxiosPrivate";
-import { useNavigate,useLocation } from "react-router-dom";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Users = () => {
-  const [users, setUsers] = useState();
-  const axiosPrivate = useAxoisPrivate();
+  const [users, setUsers] = useState([]);
+  const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -13,39 +13,47 @@ const Users = () => {
     const controller = new AbortController();
 
     const getUsers = async () => {
-      try{
-        const response = await axiosPrivate.get('/users', {
-          signal: controller.signal
+      try {
+        const response = await axiosPrivate.get("/users", {
+          signal: controller.signal,
         });
+        const userNames=response.data.map(user=>user.username);
         console.log(response.data);
-        isMounted && setUsers(response.data);
-      }catch(err){
-        console.error(err);
-        navigate('/login', { state: { from: location }, replace: true });
+        if (isMounted) {
+          setUsers(userNames);
+        }
+      } catch (err) {
+        if (err.name === 'CanceledError') {
+          console.log('Request was canceled');
+        } else {
+          console.error(err);
+          navigate("/login", { state: { from: location }, replace: true });
+        }
       }
-    }
+    };
 
     getUsers();
 
     return () => {
       isMounted = false;
       controller.abort();
-    }
-  },[])
+    };
+  }, [axiosPrivate, navigate, location]);
+
   return (
     <article>
       <h2>Users List</h2>
-      {users?.length
-        ? (
-          <ul>
-            {users.map((user, i) =>
-              <li key={i}>{user?.username}</li>)}
-          </ul>
-        ) : <p>No users to display</p>
-      } 
-     
+      {users.length ? (
+        <ul>
+          {users.map((user, i) => (
+            <li key={i}>{user}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users to display</p>
+      )}
     </article>
-  )
-}
+  );
+};
 
 export default Users;
