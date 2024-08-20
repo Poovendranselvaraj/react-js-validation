ed4abd080713bfca9971ae990233326b98cc41ae
poove <poovendranselavaraj333@gmail.com>
2024-08-20 18:03:08 +0530

    55

--- mongo_async_crud/routes/api/users.js
+++ mongo_async_crud/routes/api/users.js
@@ -4,7 +4,8 @@ const usersController = require('../../controllers/usersController');
 const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
 
-router.route('/')
+router.route('/') 
+    .get(usersController.getAllUsers)
     .get(verifyRoles(ROLES_LIST.Admin), usersController.getAllUsers)
     .delete(verifyRoles(ROLES_LIST.Admin), usersController.deleteUser);
 
