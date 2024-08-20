30a576ba7adc605dd07b26c0b4d730f1ee76ac99
poove <poovendranselavaraj333@gmail.com>
2024-08-20 20:48:45 +0530

    9th completed

--- auth0_tut/src/components/LogoutButton.js
+++ auth0_tut/src/components/LogoutButton.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
     
     return (
         isAuthenticated && (
-            <button onClick={() => logout()}>Sign In</button>
+            <button onClick={() => logout()}>Sign Out</button>
         )
     )
 }
