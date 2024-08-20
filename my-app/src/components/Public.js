c992f8cfc1c1f6720f5242968c0cabf77b0e6321
poove <poovendranselavaraj333@gmail.com>
2024-08-20 17:19:21 +0530

    8th 50.25 somemistake

--- my-app/src/components/Public.js
+++ my-app/src/components/Public.js
@@ -7,7 +7,6 @@ const Public = () => {
             <header>
                 <h1>Welcome to Repair Store!</h1>
             </header>
-            <hr />
             <main>
                 <p>Located in Beautiful Downtown Foo City, Repair Store provides a trained staff ready to meet your repair needs.</p>
                 <p>&nbsp;</p>
@@ -19,15 +18,12 @@ const Public = () => {
                     <a href="tel:+15555555555">(555) 555-5555</a>
                 </address>
             </main>
-
-            
-            <hr />
             <footer>
                 <Link to="/login">Employee Login</Link>
             </footer>
         </section>
 
-    )
+    ) 
     return content
 }
 export default Public
